97b4acbb045c113d836f2cdf66037d254a21ea9f
naenae-b <93103874+naenae-b@users.noreply.github.com>
2022-09-20 16:48:17 +0800

    Update Ex02PhotonBadiola.java

--- Ex02PhotonBadiola.java
+++ Ex02PhotonBadiola.java
@@ -11,8 +11,8 @@ public class Ex02PhotonBadiola {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input, ansnum;
-        int range = 10, min = 1;
+        String input;
+        int max = 10, min = 1, guessCount = 3;
         do{
         System.out.println("""
                            Welcome to Higher or Lower! What will you do?
@@ -23,14 +23,39 @@ public class Ex02PhotonBadiola {
         
         input = sc.nextLine();
         
-        switch(input){
+        switch (input){
             
             case "Start game":
-                
-                int random = (int) Math.floor(Math.random()*range) + min;
-                System.out.print("You have 3 guess(es) left. What is your guess? " + random + range + min);
-                ansnum = sc.nextLine();
-                break;
+              
+                   int random = (int) Math.floor(Math.random()*(max-min)) + min;
+                   var rYes = "y";
+                   var rNo = "n";
+              
+                   for (int i = guessCount;i > 0; i--) {
+                          System.out.println("You have " + i + " guess(es)! What is your guess?");
+                          Scanner guessInput = new Scanner(System.in);
+                        int Guess = guessInput.nextInt();
+                  
+                        if (Guess == random){
+                             System.out.println("You got it!");
+                             System.out.println("Play again (y/n)?");
+                             break;
+                      
+                        } else if (Guess > random) {
+                              System.out.println("Guess lower! You have " + i + " guess(es) left! What is your guess?");
+                        } else if (Guess < random) {
+                       System.out.println("Guess higher! You have " + i + " guess(es) left! What is your guess?");
+                         }
+                      }
+              
+                   Scanner reInput = new Scanner(System.in);
+                 String replay = reInput.nextLine();
+                    if (replay.equals(rYes))
+                             continue;
+                    if (replay.equals (rNo))
+                            System.out.println("\nThank you for playing!");
+                            break;
+                 
                 
             case "Change settings": 
                 System.out.print("""
@@ -38,6 +63,22 @@ public class Ex02PhotonBadiola {
                                  What is the upper limit of the random number?
                                  How many guesses are allowed?
                                  """);
+                
+              System.out.println("Lower limit: ");
+              Scanner lowerLimit = new Scanner(System.in);
+              min = lowerLimit.nextInt();
+              
+              System.out.println("Upper limit: ");
+              Scanner upperLimit = new Scanner(System.in);
+              max = upperLimit.nextInt();
+              
+              System.out.println("Guesses: ");
+              Scanner guessLimit = new Scanner(System.in);
+              guessCount = guessLimit.nextInt();
+                break;
+            
+            case "End application":
+                System.out.print("\nThank you for playing!\n");
                 break;
                 
         }
